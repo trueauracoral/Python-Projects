091bab502a5e7d96d328d01c1b1ecd7dd4141ccf
true-aura-coral <true-aura-coral@noreply.codeberg.org>
2021-11-04 04:30:24 +0100

    Copys to clipboard

--- noodysee.py
+++ noodysee.py
@@ -1,3 +1,18 @@
+#####################################################################
+#                                                                   #
+# DESCRIPTION:                                                      #
+# This is a file for changing odysee links into a chosen link in the#
+# variables.                                                        #
+#                                                                   #
+# LISCENSE INFO (A dedicated liscense file is in the repo.):        #
+#      ALL THE CODE IN THIS REPOSITORY INCLUDING THIS FILE IS       #
+# (C) TrueAuraCoral and Other Contributors 2021.                    #
+# YOU CAN USE THIS FILE AND ANY OTHER FILE IN THIS REPOSITORY UNDER #
+# THE TERMS OF GNU GENERAL PUBLIC LICENSE VERSION 3 OR ANY LATER    #
+# VERSION. TO FIND THE FULL TEXT OF THE LICENSE GO TO THE GNU.ORG   #
+# WEBSITE AT ( https://www.gnu.org/licenses/gpl-3.0.html ).         #
+#####################################################################
+
 # For command line args
 import sys
 # For running commands in shell with subprocess.Popen
@@ -24,5 +39,8 @@ browser = 'c:\\users\\stanl\\appData\\local\\bravesoftware\\brave-browser\\appli
 
 # Only replace once
 new = link.replace("odysee.com", instance, 1)
+
+# Copy to clipboard
+subprocess.run('clip', universal_newlines=True, input=new)
 
 subprocess.Popen([browser, new])
