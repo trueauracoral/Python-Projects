1d5ce53ca538c03652c2bff00c9c83ea4be39ffb
true-aura-coral <true-aura-coral@noreply.codeberg.org>
2021-11-10 16:13:28 +0100

    A actually useful script.

---
+++ lbctousd.py
@@ -0,0 +1,10 @@
+# This is a plugin for converting lbc to usd
+# (c) TrueAuraCoral 2021 - under GNU GPLv3 or any later version.
+
+# Make sure you pip install this
+import requests
+# Get how much lbc
+lbc = input("How much LBC? ")
+
+# Print it out
+print(requests.get("https://rate.sx/" + lbc + "LBC").text.rstrip())
