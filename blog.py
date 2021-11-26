89ce7496aae9ab1b1c2248298864a3f71aba7591
zortazert <zortazert@noreply.codeberg.org>
2021-11-26 21:19:00 +0100

    Multiple f.write's for readability.

--- blog.py
+++ blog.py
@@ -20,5 +20,16 @@ with open(rssfile, "w") as w:
 
 # Add stuff
 with open(rssfile, 'a') as f:
-    f.write("\n<item>\n<title>"+Title+"</title>\n<link>"+Link+"</link>\n<guid>"+Link+"</guid>\n<pubDate>"+Date+"</pubDate>\n<description>\n<![CDATA[ You can consume this content in any app you like! ]]>\n</description>\n</item>\n\n</channel>\n</rss>")
+    f.write("\n<item>")
+    f.write("\n<title>"+Title+"</title>")
+    f.write("\n<link>"+Link+"</link>")
+    f.write("\n<guid>"+Link+"</guid>")
+    f.write("\n<pubDate>"+Date+"</pubDate>")
+    f.write("\n<description>")
+    f.write("\n<![CDATA[ You can consume this content in any app you like! ]]>")
+    f.write("\n</description>")
+    f.write("\n</item>")
+    f.write("\n")
+    f.write("\n</channel>")
+    f.write("\n</rss>")
     # f.write("\n<item>\n<title>"+Title+"</title>\n<link>"+Link+"</link>\n<guid>"+Link+"</guid>\n<pubDate>"+Date+"</pubDate>\n<description>\n"+htmlfile+"\n</description>\n</item>\n\n</channel>\n</rss>")
