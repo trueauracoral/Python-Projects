0b8c6d19936cc444f8962ed06276167a671a0bb2
zortazert <zortazert@noreply.codeberg.org>
2021-11-26 19:22:55 +0100

    It now deletes channel and rss tags.

--- blog.py
+++ blog.py
@@ -1,13 +1,22 @@
+# I need to get time instead of asking manuelly for it.
+from time import gmtime, strftime
+
 # Inputs
 Title = input("Title: ")
 Link = input("Link: ")
-Date = input("Date: ")
+Date = str(strftime("%a, %d %b %Y %X"))
 htmlfile = input("HTML file: ")
 rssfile = "rss.xml"
 
 # cdata ~> Description ~> Reads in rss reader I hope :-)
 htmlfile = str("<![CDATA[ "+open(htmlfile).read().replace('\n', ' ')+" ]]>")
 
+# Delete channel and rss tags
+with open(rssfile) as r:
+  text = r.read().replace("\n</channel>\n</rss>", " ")
+with open(rssfile, "w") as w:
+  w.write(text)
+
 # Add stuff
 with open(rssfile, 'a') as f:
-    f.write("\n<item>\n<title>"+Title+"</title>\n<link>"+Link+"</link>\n<pubDate>"+Date+"</pubDate>\n<description>\n"+htmlfile+"\n</description>\n</item>\n")
+    f.write("\n<item>\n<title>"+Title+"</title>\n<link>"+Link+"</link>\n<guid>"+Link+"</guid>\n<pubDate>"+Date+"</pubDate>\n<description>\n"+htmlfile+"\n</description>\n</item>\n\n</channel>\n</rss>")
