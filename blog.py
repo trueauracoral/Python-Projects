8fc2932757bb2587d6c774e33db2744179a4197a
zortazert <zortazert@noreply.codeberg.org>
2021-11-26 02:55:17 +0100

    Completely new script.
    # What I got to do
    - Add 3 lines above end
    - Clean up html stuff that isn't <p>, <i> <b>, <img> (many more tags I don't know)

--- blog.py
+++ blog.py
@@ -1,75 +1,13 @@
-# Version 0 by TrueAuraCoral
-# # File to create blogs but mostly just generate rss entries for me
-#
-# # Ask for input and stuff for the entries
-# title = input("Title: ")
-# link = input("Link: ")
-# date = input("Pubdate: ")
-#
-# # Use a multiple print to print both variables that I asked for input and print text.
-# # I chose to use "+" variabe "+" as seperators. I seem to like this.
-# print("<--------------COPY COPY COPY-------------->")
-#
-# print("<item>\n<title>" + title + "</title>")
-#
-# print("<link>" + link + "</link>")
-#
-# print("<pubDate>" + date + "</pubDate>")
-#
-# print("</item>")
+# Inputs
+Title = input("Title: ")
+Link = input("Link: ")
+Date = input("Date: ")
+htmlfile = input("HTML file: ")
+rssfile = "rss.xml"
 
-# Version 1 by Duckey
-# Ask for input and stuff for the entries
-# title = input("Title: ")
-# link = input("Link: ")
-# date = input("Pubdate: ")
-# file = input("Paste to: ")
-#
-#
-# file = open(file, "w")
-# file.writelines("<item>\n<title>" + title + "</title>\n")
-#
-# file.writelines("<link>" + link + "</link>\n")
-#
-# file.writelines("<pubDate>" + date + "</pubDate>\n")
-#
-# file.writelines("</item>\n")
+# cdata ~> Description ~> Reads in rss reader I hope :-)
+htmlfile = str("<![CDATA[ "+open(htmlfile).read().replace('\n', ' ')+" ]]>")
 
-# Version 2 GalacticColourisation#5721
-# dat = {"title": None, "link": None, "date": None, "file": None}
-# for k, v in dat.items():
-#     dat[k] = input(f"{k}: ")
-#
-# with open(dat["file"], "r+") as file:
-#     contents = file.readlines()[:-2]  # remove last two lines
-#     contents.append(f"<item>\n<title>{dat['title']}</title>\n")
-#     contents.append(f"<pubDate>{dat['date']}</pubDate>\n")
-#     contents.append("</item>\n")
-#     contents.append("</channel>\n</rss>\n\n\n")
-#     file.seek(0)
-#     file.truncate()
-#     file.write(''.join(contents))
-
-# Version 3 GalacticColourisation#5721
-import textwrap
-
-dat = {"title": None, "link": None, "date": None, "file": None}
-for k, v in dat.items():
-    dat[k] = input(f"{k}: ")
-
-with open(dat["file"], "r+") as file:
-    contents = ''.join(file.readlines()[:-2])  # remove last two lines
-    contents += textwrap.dedent(
-        f"""
-        <item>
-        <title>{dat['title']}</title>
-        <link>{dat['link']}</link>
-        <pubDate>{dat['date']}</pubDate>
-        </item>\n\n\n
-        </channel>
-        </rss>
-        """
-    )
-    file.seek(0)
-    file.truncate()
-    file.write(contents)
+# Add stuff
+with open(rssfile, 'a') as f:
+    f.write("\n<item>\n<title>"+Title+"</title>\n<link>"+Link+"</link>\n<pubDate>"+Date+"</pubDate>\n<description>\n"+htmlfile+"\n</description>\n</item>\n")
