a7e3d48bffd38cb58aa6e6650b2925d735575bf3
zortazert <zortazert@noreply.codeberg.org>
2021-12-01 17:50:17 +0100

    I just added spaces.

--- blog.py
+++ blog.py
@@ -12,9 +12,12 @@ rssfile = "rss.xml"
 # I need to figure out how to get data from <p> in html file
 # htmlfile = str("<![CDATA[ "+open(htmlfile).read().replace('\n', ' ')+" ]]>")
 
-# Delete channel and rss tags
+# Delete channel and rss tags A very wonky and odd solution to the
+# issue of this text being early in the file is to litterally add
+# spaces to the end of the tags so it's very specific in what it's
+# looking for.
 with open(rssfile) as r:
-  text = r.read().replace("\n</channel>\n</rss>", " ")
+  text = r.read().replace("\n</channel>   \n</rss>     ", " ")
 with open(rssfile, "w") as w:
   w.write(text)
 
@@ -30,6 +33,12 @@ with open(rssfile, 'a') as f:
     f.write("\n</description>")
     f.write("\n</item>")
     f.write("\n")
-    f.write("\n</channel>")
-    f.write("\n</rss>")
+    f.write("\n</channel>   ")
+    f.write("\n</rss>     ")
     # f.write("\n<item>\n<title>"+Title+"</title>\n<link>"+Link+"</link>\n<guid>"+Link+"</guid>\n<pubDate>"+Date+"</pubDate>\n<description>\n"+htmlfile+"\n</description>\n</item>\n\n</channel>\n</rss>")
+
+# Print file so I can glance and check for errors.
+f = open('rss.xml', 'r')
+content = f.read()
+print(content)
+f.close()
