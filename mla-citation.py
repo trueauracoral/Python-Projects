39cd30f4932ca4f5926208ebf48cd15b4fbe6b63
zortazert <zortazert@noreply.codeberg.org>
2021-12-01 19:02:01 +0100

    CITATIONS ARE NOW MORE EPIC

---
+++ mla-citation.py
@@ -0,0 +1,11 @@
+Title = input("Title: ")
+Who = input("By who? : ")
+PubDate = input("Publication Date: ")
+Link = input("Link: ")
+
+print(" ________________________________________________")
+print("|                                                |")
+print("|     Here is your citation. Please copy it.     |")
+print("|________________________________________________|\n")
+
+print(Title+", "+Who+", "+PubDate+", "+Link+" ")
