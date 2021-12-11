3a6d6d20b95f15712bc0b5c4a558cda719653a12
zortazert <zortazert@noreply.codeberg.org>
2021-12-11 23:38:24 +0100

    Comments are now multi line.

--- thumbnail/thumbnail.py
+++ thumbnail/thumbnail.py
@@ -1,8 +1,11 @@
-# This is a script for me to generate video thumbnails for none video publications. I am way to lazy to create thumbnails so this just does it automaticly.
+# This is a script for me to generate video thumbnails for none video
+# publications. I am way to lazy to create thumbnails so this just
+# does it automaticly.
 
 # Python image processing imports. Handling text and other things.
 from PIL import Image, ImageDraw, ImageFont
-# This if for text wrapping. Using \n isn't supported in PIL text stuff.
+# This if for text wrapping. Using \n isn't supported in PIL text
+# stuff.
 import textwrap
 
 # Image open and draw
@@ -15,7 +18,8 @@ textwrapped = textwrap.wrap(text, width=30)
 dubai = ImageFont.truetype("ReemKufi-Regular.ttf", 100)
 draw.text((400,400), '\n'.join(textwrapped), font=dubai, fill="black")
 
-# I do some manipulation on the text variable so I get a nicer file name.
+# I do some manipulation on the text variable so I get a nicer file
+# name.
 filename = text.replace(' ', '-')+".png"
 image.save(filename, "PNG")
 image.show()
