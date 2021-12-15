af59610771596f4f78b9a05b550b314483c39d46
zortazert <zortazert@noreply.codeberg.org>
2021-12-15 07:50:20 +0100

    A lot more added to this script.

--- pass.py
+++ pass.py
@@ -1,6 +1,16 @@
 import random
 import string
 
+# Gives me a password.
 keys = (string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation)
+phrase = ("".join(random.sample(keys,16)))
 
-print("".join(random.sample(keys,16)))
+# Get some data about what is this account
+account = input("Account: ")
+username = input("Username: ")
+
+# Open org file and start adding things
+with open("pasec.org", 'a') as f:
+    f.write("\n* " + account)
+    f.write("\n- Username: " + username)
+    f.write("\n- Pass: " + phrase)
