0b464b938b56acd06ff04cc216a7949f1f41209b
zortazert <zortazert@matthewevan.xyz>
2022-02-04 14:24:29 -0600

    MPV!!!!!! YES YES YES YES

--- vids.py
+++ vids.py
@@ -10,7 +10,8 @@ print('''
 \\-------------------------------/
 ''')
 
-command = "vieb "
+# Use either a browser or mpv
+command = "C:\\SGZ_Pro\\z-apps_drivers\\mpv\mpv.exe "
 bold = "\033[01m"
 norm = "\033[00m"
 bright_cyan = "\033[46m"
@@ -18,11 +19,11 @@ colora = "\033[45m"
 colorb = "\033[44m"
 try:
     if sys.argv[1] == "-i":
-        invidious_instance = "https://invidio.xamh.de"
+        invidious_instance = "https://invidio.xamh.de/"
         query = input("Searching for: ")
         query = str(query)
         size = str(19)
-        invidious_search = invidious_instance + "/api/v1/search?q=" + query
+        invidious_search = invidious_instance + "api/v1/search?q=" + query
         wol_api = "https://scrap.madiator.com/api/get-lbry-video?url="
 
         data = requests.get(invidious_search)
@@ -40,17 +41,19 @@ try:
         # wol-api check
         lbry_check = requests.get(wol_api + json_stuff[c]["videoId"])
         lbry_check = json.loads(lbry_check.text)
-        librarian_instance = "https://lbry.ix.tc/"
+        # For now using odysee because yt-dlp doesn't support librarian
+        librarian_instance = "https://odysee.com/"
 
         if lbry_check["lbryurl"] == None:
-            selected_url = invidious_instance + '/watch?v=' + json_stuff[c]["videoId"]
+            # Using youtube.com sadly.
+            selected_url = "https://youtube.com/" + 'watch?v=' + json_stuff[c]["videoId"]
         else:
             selected_url = librarian_instance + lbry_check["lbryurl"]
             selected_url = selected_url.replace("#", ":")
-
         # Do stuff with it.
         os.system(command + selected_url)
         quit()
+
     elif sys.argv[1] == "-p":
         query = input("Searching for: ")
         query = str(query)
@@ -73,16 +76,17 @@ try:
                     c = 100000
 
         selected_url = json_stuff["data"][c]["url"]
-
         # Do stuff with it.
         os.system(command + selected_url)
         quit()
+
     elif sys.argv[1] == "-l":
         query = input("Searching for: ")
         query = str(query)
         size = str(30)
         search = 'https://lighthouse.lbry.com/search?s=' + query + '&include=channel,title&size=' + size
-        lbry = "https://lbry.ix.tc/"
+        # For now using odysee
+        lbry = "https://odysee.com/"
 
         data = requests.get(search)
         json_stuff = json.loads(data.text)
@@ -107,8 +111,10 @@ try:
 
         # Do stuff with it.
         url = str(lbry + selected_url["channel"] + "/" + selected_url["name"])
+        # Do stuff with it.
         os.system(command + url)
         quit()
+
 except:
     print('''
 Command:
