aa1248f23fc8738b51ef4d51872222f9ed91aba9
zortazert <zortazert@matthewevan.xyz>
2022-02-05 10:54:45 -0600

    Invidious comments and other things.

--- invidious.py
+++ invidious.py
@@ -10,21 +10,19 @@ bright_cyan="\033[46m"
 colora="\033[45m"
 colorb="\033[44m"
 
-# Variables
-instance = "https://invidio.xamh.de"
+invidious_instance = "https://invidio.xamh.de/"
 query = input("Searching for: ")
 query = str(query)
 size = str(19)
-search = instance + "/api/v1/search?q=" + query
-# Any command that you can run on your system with the url link. Possily use xdg-open for GNU/Linux systems
-command = "vieb "
+invidious_search = invidious_instance + "api/v1/search?q=" + query
+wol_api = "https://scrap.madiator.com/api/get-lbry-video?url="
+command = "C:\\SGZ_Pro\\z-apps_drivers\\mpv\\mpv.exe "
 
-data = requests.get(search)
+data = requests.get(invidious_search)
 json_stuff = json.loads(data.text)
 for i, vid in enumerate(json_stuff):
     print(i, colora+vid["title"]+norm+"\n"+colorb+vid["author"]+norm+"\n"+bright_cyan+vid["videoId"]+norm)
 
-# Choose a result
 c = 100000
 while not c >= 0 or not c <= 19:
     c = input('Number from 1-' + size + " of the URL you want to open: ")
@@ -32,8 +30,24 @@ while not c >= 0 or not c <= 19:
             c = int(c)
     except:
             c = 100000
+# wol-api check
+lbry_check = requests.get(wol_api + json_stuff[c]["videoId"])
+lbry_check = json.loads(lbry_check.text)
+# For now using odysee because yt-dlp doesn't support librarian
+librarian_instance = "https://odysee.com/"
 
-selected_url = str(instance + '/watch?v=' + json_stuff[c]["videoId"])
+if lbry_check["lbryurl"] == None:
+    # Using youtube.com since yt-dlp on any given invidious url redirects to youtube.com anyways.
+    selected_url = "https://youtube.com/watch?v=" + json_stuff[c]["videoId"]
+else:
+    selected_url = librarian_instance + lbry_check["lbryurl"]
+    selected_url = selected_url.replace("#", ":")
+comments = invidious_instance + "/api/v1/comments/" + json_stuff[c]["videoId"]
+data_comment = requests.get(comments)
+json_comment = json.loads(data_comment.text)
+for i, comment in enumerate(json_comment["comments"]):
+    print(i, colora+comment["author"]+norm+"\n"+colorb+comment["content"]+norm)
 
 # Do stuff with it.
 os.system(command + selected_url)
+quit()
--- vids.py
+++ vids.py
@@ -47,10 +47,16 @@ try:
 
         if lbry_check["lbryurl"] == None:
             # Using youtube.com since yt-dlp on any given invidious url redirects to youtube.com anyways.
-            selected_url = "https://youtube.com/" + 'watch?v=' + json_stuff[c]["videoId"]
+            selected_url = "https://youtube.com/watch?v=' + json_stuff[c]["videoId"]
         else:
             selected_url = librarian_instance + lbry_check["lbryurl"]
             selected_url = selected_url.replace("#", ":")
+        comments = invidious_instance + "/api/v1/comments/" + json_stuff[c]["videoId"]
+        data_comment = requests.get(comments)
+        json_comment = json.loads(data_comment.text)
+        for i, comment in enumerate(json_comment["comments"]):
+            print(i, colora+comment["author"]+norm+"\n"+colorb+comment["content"]+norm)
+
         # Do stuff with it.
         os.system(command + selected_url)
         quit()
@@ -60,11 +66,14 @@ try:
         query = str(query)
         size = str(19)
         search = "https://sepiasearch.org/api/v1/search/videos?search=" + query
+        command = "C:\\SGZ_Pro\\z-apps_drivers\\mpv\\mpv.exe "
+
         data = requests.get(search)
         json_stuff = json.loads(data.text)
         for i, vid in enumerate(json_stuff["data"]):
             print(i, colora+vid["name"]+norm+"\n"+colorb+vid["channel"]["displayName"]+norm+"\n"+bright_cyan+vid["url"]+norm)
 
+        # Choose a result
         c = 100000
         while not c >= 0 or not c <= 19:
             c = input('Number from 1-' + size + " of the URL you want to open: ")
@@ -74,8 +83,16 @@ try:
                     c = 100000
 
         selected_url = json_stuff["data"][c]["url"]
+        comments = "https://" + json_stuff["data"][c]["account"]["host"] + "/api/v1/videos/" + json_stuff["data"][c]["uuid"] + "/comment-threads"
+        data_comment = requests.get(comments)
+        json_comment = json.loads(data_comment.text)
+        for i, comment in enumerate(json_comment["data"]):
+            if comment["account"] == None:
+                print(i, "No Account Data")
+            else:
+                #print(i, comment["account"]["displayName"])
+                print(i, colora+comment["account"]["displayName"]+norm+"\n"+colorb+comment["text"]+norm)
         os.system(command + selected_url)
-        quit()
 
     elif sys.argv[1] == "-l":
         query = input("Searching for: ")
