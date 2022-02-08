568f379e3ba88989d183952191d5a4821901b774
zortazert <zortazert@matthewevan.xyz>
2022-02-07 20:35:43 -0600

    Peertube replies (sort of)

--- sepia.py
+++ sepia.py
@@ -32,13 +32,24 @@ while not c >= 0 or not c <= 19:
             c = 100000
 
 selected_url = json_stuff["data"][c]["url"]
-comments = "https://" + json_stuff["data"][c]["account"]["host"] + "/api/v1/videos/" + json_stuff["data"][c]["uuid"] + "/comment-threads"
+comments = "https://" + json_stuff["data"][c]["account"]["host"] + "/api/v1/videos/" + json_stuff["data"][c]["uuid"] + "/comment-threads/"
 data_comment = requests.get(comments)
 json_comment = json.loads(data_comment.text)
 for i, comment in enumerate(json_comment["data"]):
     if comment["account"] == None:
         print(i, "No Account Data")
+    elif comment["totalReplies"] > 0:
+        replies = comments + str(comment["id"])
+        data_replies = requests.get(replies)
+        json_replies = json.loads(data_replies.text)
+        total_replies = str(json_replies["comment"]["totalReplies"])
+        print(i, colora+comment["account"]["displayName"]+norm+"\n"+colorb+comment["text"]+norm+bright_cyan+"\nREPLIES: "+total_replies+norm)
+        for i, reply in enumerate(json_replies["children"]):
+            if reply["comment"]["account"] == None:
+                print(i, "No Account Data")
+            else:
+                print(" " + str(i) + " " + colora+reply["comment"]["account"]["displayName"]+norm+"\n "+colorb+reply["comment"]["text"]+norm)
     else:
-        #print(i, comment["account"]["displayName"])
         print(i, colora+comment["account"]["displayName"]+norm+"\n"+colorb+comment["text"]+norm)
-os.system(command + selected_url)
+
+#os.system(command + selected_url)
