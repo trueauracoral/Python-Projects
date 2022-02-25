08c8ac1ad3fcf64bbea94b98f92b8c7a1099bbcb
zortazert <zortazert@matthewevan.xyz>
2022-02-25 10:48:26 -0600

    Search limits.

--- private-link-bot.py
+++ private-link-bot.py
@@ -1,5 +1,5 @@
 """
-A detector of twitter, reddit and youtube links that will then print out safer sites that the user can use.
+At it's core a detector of twitter, reddit and youtube links that will then print out safer sites that the user can use.
 
 PROBLEMS:
 - Annoying
@@ -28,6 +28,8 @@ Commands:
 Print out this help message.
 - fossbot-youtube <qeury>
 Print out youtube search results as invidious links with title and channel information. Uses the invidious API
+- fossbot-peertube <qeury>
+Print out peertube search results as peertube instance links with title and channel information. Uses the sepiasearch API
 - fossbot-lbry <query>
 Print out LBRY search results as librarian links with title and channel information. Uses the lighthouse API
 - fossbot-crypto <query>
@@ -35,6 +37,7 @@ Print out in USD values of crypto currencies. Uses the rate.sx API.
 - fossbot-neow <query>
 Print out the amount of neow coins a user on neow has.
 """)
+    print(event['sender']+"\nPosted:\n"+event["content"]["body"])
 
 def neow_callback(room, event):
     message = event["content"]["body"].replace("fossbot-neow ","")
@@ -42,6 +45,7 @@ def neow_callback(room, event):
     neow_search = "https://neow.matthewevan.xyz/user/" + query + "/neowcoins.txt"
     data = requests.get(neow_search).text.rstrip()
     room.send_text(query + " has " + data + " coins")
+    print(event['sender']+"\nPosted:\n"+event["content"]["body"])
 
 def crypto_callback(room, event):
     message = event["content"]["body"].replace("fossbot-crypto ","")
@@ -49,6 +53,20 @@ def crypto_callback(room, event):
     ratesx_search = "https://rate.sx/" + query
     data = requests.get(ratesx_search).text.rstrip()
     room.send_text("In USD that's " + data)
+    print(event['sender']+"\nPosted:\n"+event["content"]["body"])
+
+def peertube_callback(room, event):
+    message = event["content"]["body"].replace("fossbot-peertube ","")
+    query = message
+    peertube_search = "https://sepiasearch.org/api/v1/search/videos?search=" + query
+    data = requests.get(peertube_search)
+    json_stuff = json.loads(data.text)
+    room.send_text("Searching PeerTube for: " + query)
+    for index, vid in enumerate(json_stuff["data"]):
+        room.send_text(vid["name"]+"\n"+vid["channel"]["displayName"]+"\n"+vid["url"])
+        if index == 4:
+            break
+    print(event['sender']+"\nPosted:\n"+event["content"]["body"])
 
 def invidious_callback(room, event):
     message = event["content"]["body"].replace("fossbot-youtube ","")
@@ -57,9 +75,11 @@ def invidious_callback(room, event):
     data = requests.get(invidious_search)
     json_stuff = json.loads(data.text)
     room.send_text("Searching youtube for: " + query)
-    for vid in json_stuff:
+    for index, vid in enumerate(json_stuff):
         room.send_text(vid["title"]+"\n"+vid["author"]+"\n"+"https://invidio.xamh.de/watch?v="+vid["videoId"])
-
+        if index == 4:
+            break
+    print(event['sender']+"\nPosted:\n"+event["content"]["body"])
 def lbry_callback(room, event):
     size = str(5)
     message = event["content"]["body"].replace("fossbot-lbry ","")
@@ -76,18 +96,22 @@ def lbry_callback(room, event):
             pre += x["channel"] + "/"
         url = pre + x["name"]
         room.send_text(x["title"]+"\n"+url)
+    print(event['sender']+"\nPosted:\n"+event["content"]["body"])
 
 def youtube_callback(room, event):
     message = event["content"]["body"].split("/")
     room.send_text("Hi, " + event['sender'] + " here is a invidious link of the YouTube link you just sent:\nhttps://invidio.xamh.de/"+message[3])
+    print(event['sender']+"\nPosted:\n"+event["content"]["body"])
 
 def twitter_callback(room, event):
     message = event["content"]["body"]
     room.send_text("Hi, " + event['sender'] + " here is a nitter link of the Twitter link you just sent:\n"+message.replace("twitter.com","nitter.net"))
+    print(event['sender']+"\nPosted:\n"+event["content"]["body"])
 
 def reddit_callback(room, event):
     message = event["content"]["body"]
     room.send_text("Hi, " + event['sender'] + " here is a teddit link of the Reddit link you just sent:\n"+message.replace("reddit.com","teddit.net"))
+    print(event['sender']+"\nPosted:\n"+event["content"]["body"])
 
 def main():
     # Create an instance of the MatrixBotAPI
@@ -114,6 +138,8 @@ def main():
     bot.add_handler(neow_handler)
     help_handler = MRegexHandler("^fossbot-help", help_callback)
     bot.add_handler(help_handler)
+    peertube_handler = MRegexHandler("^fossbot-peertube", peertube_callback)
+    bot.add_handler(peertube_handler)
 
     bot.start_polling()
     # Infinitely read stdin to stall main thread while the bot runs in other threads
