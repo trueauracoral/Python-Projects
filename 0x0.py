bf964affa497eed55e45591bd58d9f0b6dcc73f1
zortazert <zortazert@matthewevan.xyz>
2022-03-09 15:36:35 -0600

    Made script better and added linux support. My eyes are dead.

--- 0x0.py
+++ 0x0.py
@@ -1,9 +1,26 @@
 import os
+import subprocess
+import sys
+import platform
 
-file = input("File: ")
-print(file)
+red = "\033[41m"
+norm = "\033[00m"
 
-linux_shell = "%LocalAppData%\\Programs\\Git\\bin\\sh.exe"
+file = ""
+try:
+    file = sys.argv[1]
+except:
+    while not file:
+        file = input("File: ")
 
-command = linux_shell+" -c \"curl -F'file=@"+file+"' http:/0x0.st\""
-os.system(command)
+print(f"Uploading.... {file}")
+
+platform = platform.system()
+
+if platform == "Windows":
+    git_bash_shell = "%LocalAppData%\\Programs\\Git\\bin\\sh.exe"
+    windows_command = f"{git_bash_shell} -c \"curl -F'file=@{file}' http:/0x0.st\""
+    os.system(windows_command)
+else:
+    linux_command = f"curl -F'file=@{file}' http:/0x0.st"
+    os.system(linux_command)
