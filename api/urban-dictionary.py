8b342ad8963ebfe75e79fc0a3554897f34a44fc3
zortazert <zortazert@matthewevan.xyz>
2022-03-12 19:27:14 -0600

    based upgrade this is more software like.

--- api/urban-dictionary.py
+++ api/urban-dictionary.py
@@ -1,10 +1,20 @@
 import requests
 import json
-query = input("Word: ")
+import sys
+
+bold = "\033[01m"
+norm = "\033[00m"
+query = ""
+try:
+    query = sys.argv[1:]
+except:
+    while not query:
+        query = input("Searching for: ")
+
 api = f"https://api.urbandictionary.com/v0/define?term={query}"
 data = requests.get(api)
 json_stuff = json.loads(data.text)
 for i, result in enumerate(json_stuff["list"]):
-    print(result["word"]+"\n"+result["definition"][:128].replace("\n","")+"..."+"\n"+result["example"][:128].replace("\n","")+"...")
+    print(bold+"Word: "+norm+result["word"]+"\n"+bold+"Definition: "+norm+result["definition"].replace("\n","").replace("[","").replace("]","")+"\n" +bold+"Example: "+norm+result["example"].replace("\n","").replace("[","").replace("]","")+"\n---")
     if i == 1:
         break
