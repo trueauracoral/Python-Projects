5414ab1aa6b615542d9f94ee1857c51ae7d05137
zortazert <zortazert@matthewevan.xyz>
2022-03-19 18:04:25 -0500

    Been a while, YT to LBRY mirror script

---
+++ lbry/yt-to-lbry.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import os
+import re
+import tempfile
+
+downloader = "C:\\SGZ\\SW_Downloads\\yt-dlp\\yt-dlp.exe"
+lbrynet = "C:\\SGZ\\SW_Downloads\\lyberry_qt\\lbrynet.exe"
+
+temp_dir = tempfile.TemporaryDirectory().name
+url = "https://pipedapi.kavin.rocks/channel/UCu17Sme-KE87ca9OTzP0p7g"
+data = requests.get(url)
+json_stuff = json.loads(data.text)
+id = str(json_stuff["relatedStreams"][0]["url"]).replace("/watch?v=","")
+video = requests.get("https://pipedapi.kavin.rocks/streams/"+id)
+video_json = json.loads(video.text)
+title = video_json["title"]
+name_thumb = re.sub(r'[\W_]+','', str(title)) + str(123)
+name = re.sub(r'[\W_]+','', str(title))
+
+description = (f"""---
+This is a LBRY mirror of of this video:
+{title}
+Original YT URL (THIS IS SPYWARE): https://youtube.com/watch?v={id}
+---
+{video_json["description"]}""")
+
+print(description)
+description = description.replace("\n","\\n")
+description = description.replace('"','\\"')
+description = description.replace("'","\\'")
+
+print("\n---\nYT download starting:")
+os.system(f"{downloader} https://youtube.com/watch?v={id}")
+
+print("\n---\nUploading thumbnail to LBRY!")
+thumbnail_data = requests.get(video_json["thumbnailUrl"])
+with open(temp_dir, 'wb') as f:
+    f.write(thumbnail_data.content)
+
+thumbnail_command = f"{lbrynet} publish --name={name_thumb} --bid=0.1 --file_path='{temp_dir}' --title='{title}' --description='{description}' --channel_name=@TrueAuraCoralPublishesImages"
+print(thumbnail_command)
+os.system(thumbnail_command)
+command = f"{lbrynet} publish --name={name} --bid=0.1 --file_path='{title} [{id}]' --title={title} --description={description} --channel_name=@TrueAuraCoralPublishesImages --thumbnail=https://spee.ch/@TrueAuraCoralPublishesImages/{name_thumb}"
+print(command)
+os.system(command)
