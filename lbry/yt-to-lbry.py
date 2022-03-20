ea23fee4278b4a9e48e2d9743dbcf19e48e08bee
zortazert <zortazert@matthewevan.xyz>
2022-03-20 12:07:47 -0500

    no long file path, just yt-dlp.

--- lbry/yt-to-lbry.py
+++ lbry/yt-to-lbry.py
@@ -5,7 +5,7 @@ import re
 import tempfile
 
 url = "https://pipedapi.kavin.rocks/channel/UCu17Sme-KE87ca9OTzP0p7g"
-downloader = "C:\\SGZ\\SW_Downloads\\yt-dlp\\yt-dlp.exe"
+downloader = "yt-dlp"
 lbrynet = "C:\\SGZ\\SW_Downloads\\lyberry_qt\\lbrynet.exe"
 temp_dir = tempfile.TemporaryDirectory().name
 
