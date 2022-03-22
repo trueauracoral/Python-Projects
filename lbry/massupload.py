9e3f06c66cabfffb7134998dbe0c468d58ee15fe
zortazert <zortazert@matthewevan.xyz>
2022-03-21 20:13:20 -0500

    IF windows, use weird backslash, ELSE use chad slash.

--- lbry/massupload.py
+++ lbry/massupload.py
@@ -1,12 +1,17 @@
 import os
 import re
+import platform
 
 lbrynet = "lbrynet"
 files = os.listdir()
-file_path = os.getcwd() + "\\"
+if platform.system() == "Windows":
+    slash = "\\"
+else:
+    slash = "/"
+file_path = os.getcwd() + slash
 
-#for image in files:
-#    print(image)
-#    os.system(f'{lbrynet} publish --name={image} --bid=0.1 --file_path="{file_path + image}" --title="{image}" --description="mass art upload for TrueAuraCoraL :)" --channel_name=@TrueAuraCoralPublishesImages')
+for image in files:
+    print(image)
+    os.system(f'{lbrynet} publish --name={image} --bid=0.1 --file_path="{file_path + image}" --title="{image}" --description="mass art upload for TrueAuraCoraL :)" --channel_name=@TrueAuraCoralPublishesImages')
 for image in files:
     print(f"https://spee.ch/@TrueAuraCoralPublishesImages/{image}")
--- lbry/yt-to-lbry.py
+++ lbry/yt-to-lbry.py
@@ -3,6 +3,7 @@ import json
 import os
 import re
 import tempfile
+import platform
 
 url = "https://pipedapi.kavin.rocks/channel/UCu17Sme-KE87ca9OTzP0p7g"
 downloader = "yt-dlp"
@@ -41,7 +42,11 @@ with open(temp_dir, 'wb') as f:
 thumbnail_command = f'{lbrynet} publish --name={name_thumb} --bid=0.1 --file_path="{temp_dir}" --title="{title}" --description="{description}" --channel_name=@TrueAuraCoralPublishesImages'
 print(thumbnail_command)
 os.system(thumbnail_command)
+if platform.system() == "Windows":
+    slash = "\\"
+else:
+    slash = "/"
 cwd = os.getcwd()
-command = f'{lbrynet} publish --name={name} --bid=0.1 --file_path="{cwd}\\{title} [{id}].mp4" --title="{title}" --description="{description}" --channel_name=@TrueAuraCoralPublishesImages --thumbnail="https://spee.ch/@TrueAuraCoralPublishesImages/{name_thumb}"'
+command = f'{lbrynet} publish --name={name} --bid=0.1 --file_path="{cwd}{slash}{title} [{id}].mp4" --title="{title}" --description="{description}" --channel_name=@TrueAuraCoralPublishesImages --thumbnail="https://spee.ch/@TrueAuraCoralPublishesImages/{name_thumb}"'
 print(command)
 os.system(command)
