d72b0a431b5c6430b82bc16b89e4f1f6dbda29ad
zortazert <zortazert@matthewevan.xyz>
2022-03-21 19:47:52 -0500

    This thing now works. At least testing on windows 10.

--- lbry/yt-to-lbry.py
+++ lbry/yt-to-lbry.py
@@ -38,9 +38,10 @@ thumbnail_data = requests.get(video_json["thumbnailUrl"])
 with open(temp_dir, 'wb') as f:
     f.write(thumbnail_data.content)
 
-thumbnail_command = f"{lbrynet} publish --name={name_thumb} --bid=0.1 --file_path='{temp_dir}' --title='{title}' --description='{description}' --channel_name=@TrueAuraCoralPublishesImages"
+thumbnail_command = f'{lbrynet} publish --name={name_thumb} --bid=0.1 --file_path="{temp_dir}" --title="{title}" --description="{description}" --channel_name=@TrueAuraCoralPublishesImages'
 print(thumbnail_command)
 os.system(thumbnail_command)
-command = f"{lbrynet} publish --name={name} --bid=0.1 --file_path='{title} [{id}]' --title={title} --description={description} --channel_name=@TrueAuraCoralPublishesImages --thumbnail=https://spee.ch/@TrueAuraCoralPublishesImages/{name_thumb}"
+cwd = os.getcwd()
+command = f'{lbrynet} publish --name={name} --bid=0.1 --file_path="{cwd}\\{title} [{id}].mp4" --title="{title}" --description="{description}" --channel_name=@TrueAuraCoralPublishesImages --thumbnail="https://spee.ch/@TrueAuraCoralPublishesImages/{name_thumb}"'
 print(command)
 os.system(command)
