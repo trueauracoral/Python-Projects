10601c33d42b1c27ec117cec19b25e3a322ff8d6
zortazert <zortazert@matthewevan.xyz>
2022-04-03 23:07:14 -0500

    knowyourmeme is now more complete.

--- api/knowyourmeme.py
+++ api/knowyourmeme.py
@@ -2,7 +2,14 @@ import requests
 import re
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-query = "linux"
+query = ""
+try:
+    query = sys.argv[1]
+    print(query)
+except:
+    while not query:
+        query = input("Searching for: ")
+
 search = f"https://knowyourmeme.com/search?q={query}"
 search_result = requests.get(search, headers=headers)
 search_data = str(search_result.content)
@@ -18,15 +25,14 @@ for i, link in enumerate(links[15:]):
 
 c = int(input(f"Which meme from 0-{i}: "))
 selected = "https://knowyourmeme.com"+links[c+15].replace("<a href=\"", "").replace("\">", "")
-print(selected)
-#url = 'https://knowyourmeme.com/memes/linus-selfie'
-#result = requests.get(url, headers=headers)
-#data = str(result.content)
-#
-#paragraph = re.findall('<p>(.+?)</p>',data)
-#cleaner = re.compile('<.*?>')
-#about = re.sub(cleaner, '', paragraph[2])
-#print("About: " + about)
-#origin = re.sub(cleaner, '', paragraph[3])
-#print("\nOrigin: " + origin)
-#spread = re.sub(cleaner, '', paragraph[4])
+
+result = requests.get(selected, headers=headers)
+data = str(result.content)
+
+paragraph = re.findall('<p>(.+?)</p>',data)
+cleaner = re.compile('<.*?>')
+about = re.sub(cleaner, '', paragraph[2])
+print("About: " + about)
+origin = re.sub(cleaner, '', paragraph[3])
+print("\nOrigin: " + origin)
+spread = re.sub(cleaner, '', paragraph[4])
