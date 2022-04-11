4398104dd8f3ca1ed658cd48bc48288b2a2c9d67
zortazert <zortazert@matthewevan.xyz>
2022-04-11 15:32:26 -0500

    tell fortunes

--- snakesay.py
+++ snakesay.py
@@ -1,5 +1,7 @@
 import sys
 import textwrap
+import json
+import requests
 
 chadoku = """
                          ___
@@ -13,21 +15,24 @@ chadoku = """
 
 if len(sys.argv) == 1:
     print("please give an argument to print text")
+    quit()
+if sys.argv[1] == "fortune":
+    text = requests.get("https://helloacm.com/api/fortune/").json().replace("      "," ")
 else:
     text = ' '.join(sys.argv[1:])
-    num = len(text)
-    dashes = num + 2
-    if dashes > 19:
-        dashes = 21
-        print(" " + "-"*dashes)
-        text = textwrap.fill(text, 19).split("\n")
-        for enter in text:
-            stuff = "| " + enter
-            length = 21 - len(stuff)
-            print(stuff + " "*length+" |")
-        print(" " + "-"*dashes)
-    else:
-        print(" " + "-"*dashes)
-        print("| " + text + " |")
-        print(" " + "-"*dashes)
-    print(chadoku)
+num = len(text)
+dashes = num + 2
+if dashes > 19:
+    dashes = 42
+    print(" " + "-"*dashes)
+    text = textwrap.fill(text, dashes-2).split("\n")
+    for enter in text:
+        stuff = "| " + enter
+        length = dashes - len(stuff)
+        print(stuff + " "*length+" |")
+    print(" " + "-"*dashes)
+else:
+    print(" " + "-"*dashes)
+    print("| " + text + " |")
+    print(" " + "-"*dashes)
+print(chadoku)
