7dad55934cc5401d52bdd591991568f262b1359e
zortazert <zortazert@matthewevan.xyz>
2022-04-11 00:10:30 -0500

    Get the pixel version of pokemon as image.

---
+++ scrape/pokepixel.py
@@ -0,0 +1,27 @@
+import requests
+import re
+import sys
+import tempfile
+import random
+import os
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121201 icecat/17.0.1'}
+image_viewer = "palemoon"
+dir = "C:\\SGZ\\Pictures\\"
+
+database = "https://pokemondb.net/pokedex/all"
+request = requests.get(database, headers=headers)
+data = str(request.content)
+
+images = re.findall('<span class="img-fixed icon-pkmn" data-src=".+?>',data)
+for i, image in enumerate(images):
+    url = re.findall(r'(https?://\S+)', image.replace("\"",""))[0]
+    print(url)
+    name = re.sub('<span class="img-fixed icon-pkmn" data-src=".+?" data-alt="',"",image).replace(" icon\">","").lower()
+    if "\\" in name:
+        name = name.split("\\")[0]
+    print(name)
+
+    thumbnail_data = requests.get(url)
+    with open(dir + name + ".png", 'wb') as f:
+        f.write(thumbnail_data.content)
