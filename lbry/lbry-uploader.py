e89f489bad29027a4130a05466fefdd7ebce6058
zortazert <zortazert@matthewevan.xyz>
2022-04-10 21:26:28 -0500

    Fix lbry-uploader, made pokedex.py open image of pokemon

--- lbry/lbry-uploader.py
+++ lbry/lbry-uploader.py
@@ -84,6 +84,9 @@ try:
     thumbnail = input("Thumbnail file location: ")
 except:
     thumbnail = ""
+ 	
+				
+name_thumb = re.sub(r'[\W_]+','', str(title)) + str(123)
 
 # Publication
 print("---\nFinally we are at the last step!")
@@ -111,7 +114,7 @@ if thumbnail == "":
     os.system(command)
 else:
     print("\n---\nUploading thumbnail to LBRY!")
-    thumbnail_command = f'{lbrynet} publish --bid={bid} --file_path="{thumbnail}" --title="{title}" --description="{description}"'
+    thumbnail_command = f'{lbrynet} publish --name={name_thumb} --bid={bid} --file_path="{thumbnail}" --title="{title}" --description="{description}"'
     thumbnail_data = subprocess.getoutput(thumbnail_command)
     json_stuff = json.loads(thumbnail_data)
     thumbnail_url = json_stuff["outputs"][0]["permanent_url"].replace("lbry:/","https://spee.ch")
--- scrape/pokedex.py
+++ scrape/pokedex.py
@@ -1,9 +1,13 @@
 import requests
 import re
 import sys
+import tempfile
 import random
+import os
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121201 icecat/17.0.1'}
+image_viewer = "palemoon"
+temp_dir = tempfile.TemporaryDirectory().name
 
 query = ""
 try:
@@ -16,7 +20,19 @@ search = f"https://pokemondb.net/pokedex/{query}"
 search_result = requests.get(search, headers=headers)
 search_data = str(search_result.content)
 pokedex = re.findall('<td class="cell-med-text">(.+?)</td>',search_data)
+
 try:
-    print(links[random.randint(0,len(links))])
+    print(pokedex[random.randint(0,len(pokedex))])
 except:
     print("ERROR: Either you mispelled or this pokemon doesn't exist")
+
+try:
+    image = re.findall('<img src=(.+?)>',search_data)
+    url = re.findall(r'(https?://\S+)', image[0].replace("\"",""))
+    print(url[0])
+    thumbnail_data = requests.get(url[0])
+    with open(temp_dir, 'wb') as f:
+        f.write(thumbnail_data.content)
+    os.system(f"{image_viewer} {temp_dir}")
+except:
+    print("ERROR: Could not get image")
