be48e5cae7e6aef08c87c76d973585553e6cc4fc
zortazert <zortazert@matthewevan.xyz>
2022-04-10 22:31:15 -0500

    Pokemon image downloader. (Download all the pokemon images)

---
+++ scrape/pokeimage.py
@@ -0,0 +1,29 @@
+import requests
+import re
+import sys
+import tempfile
+import random
+import os
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121201 icecat/17.0.1'}
+image_viewer = "palemoon"
+dir = "C:\\SGZ_Pro\\Hobbys\\Media\\pokemon\\"
+
+database = "https://pokemondb.net/pokedex/national"
+request = requests.get(database, headers=headers)
+data = str(request.content)
+print(data)
+
+images = re.findall('<span class="img-fixed img-sprite" data-src=".+?>',data)
+for i, image in enumerate(images):
+    url = re.findall(r'(https?://\S+)', image.replace("\"",""))[0]
+    print(url)
+    name = re.sub('<span class="img-fixed img-sprite" data-src=".+?" data-alt="',"",image).replace(" sprite\">","").lower()
+    print(name)
+
+    thumbnail_data = requests.get(url)
+    try:
+        with open(dir + name + ".png", 'wb') as f:
+            f.write(thumbnail_data.content)
+    except:
+        print("error")
