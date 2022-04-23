e90cc62fb5d708579d02305e6add856d3626035e
zortazert <zortazert@matthewevan.xyz>
2022-04-22 20:31:30 -0500

    linchad support.

--- ffmpeg-record.py
+++ ffmpeg-record.py
@@ -2,6 +2,9 @@ import os
 
 try:
     output = input("Where do you want the recording to go: ")
-    os.system(f"ffmpeg -f gdigrab -framerate 30 -i desktop {output}")
+    if platform.system() == "Windows":
+        os.system(f"ffmpeg -f gdigrab -framerate 30 -i desktop {output}")
+    else:
+        os.system(f"ffmpeg -f x11grab -i :0.0 -f alsa -i hw:0 {output}")
 except:
     quit()
