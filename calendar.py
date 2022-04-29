1f26c06674577062a8a7512a34c0155c91cef0ef
zortazert <zortazert@matthewevan.xyz>
2022-04-29 10:29:56 -0500

    work in progress ffmpeg record script

--- calendar.py
+++ calendar.py
@@ -1,13 +1,16 @@
 import datetime
 
 mydate = datetime.datetime.now()
+first = datetime.date.today().replace(day=1)
 
 mm = mydate.strftime("%B")
 dd = mydate.strftime("%d")
+yy = mydate.strftime("%Y")
 num = 22
-num2 = len(mm+" "+dd)
+text = mm+" "+dd+", "+yy
+num2 = len(text)
 final = int((num-num2)*0.5)
-print("-"*final+mm+" "+dd+"-"*final)
+print("-"*final+text+"-"*final)
 
 print(" Su Mo Tu We Th Fr Sa ")
 if mm in ["January", "March", "May", "July", "August", "October", "December"]:
@@ -21,5 +24,7 @@ for i in range(1,day_num+1):
         print(" "+str(i)+"\n",end="")
     elif len(str(i)) == 2:
         print(" "+str(i), end="")
+    #elif str(i) == dd:
+    #    print("["+str(i)+"]",end="")
     else:
         print(" "+str(i)+" ", end="")
--- ffmpeg-record.py
+++ ffmpeg-record.py
@@ -1,26 +1,34 @@
 import os
 import subprocess
 import re
+import sys
 
-#try:
-#    output = input("Where do you want the recording to go: ")
-#    if platform.system() == "Windows":
-#        os.system(f"ffmpeg -f gdigrab -framerate 30 -i desktop {output}")
-#    else:
-#        os.system(f"ffmpeg -f x11grab -i :0.0 -f alsa -i hw:0 {output}")
-#except:
-#    quit()
+if len(sys.argv) == 1:
+    print("HA LOL")
+elif sys.argv[1] == "-s":
+    try:
+        output = input("Where do you want the recording to go: ")
+        if platform.system() == "Windows":
+            os.system(f"ffmpeg -f gdigrab -framerate 30 -i desktop {output}")
+        else:
+            os.system(f"ffmpeg -f x11grab -i :0.0 -f alsa -i hw:0 {output}")
+    except:
+        quit()
 
-devices = subprocess.getoutput("ffmpeg -list_devices true -f dshow -i dummy")
-devices = re.findall('"(.+?)"',devices)
-for i,device in enumerate(devices):
-    if device.startswith("@"):
-        pass
-    else:
+elif sys.argv[1] == "-a":
+    devices = subprocess.getoutput("ffmpeg -list_devices true -f dshow -i dummy")
+    data = re.findall('"(.+?)"',devices)
+    for i,device in enumerate(data):
         print(i, device)
-# Record Audio
-# ffmpeg -f dshow -i audio="Microphone (USB Audio Device)" output.mp3
+    pick = data[int(input("Which audio device do you want? "))]
+    if pick.startswith("@"):
+        pick = data[data.index(pick)-1]
+    output = input("Where do you want the recording to go: ")
+    command = f'ffmpeg -f dshow -i audio="{pick}" {output}'
+    try:
+        os.system(command)
+    except:
+        quit()
 
-# Record video
-# ffmpeg -f gdigrab -framerate 30 -i desktop output.mp4
+# Audio & Video record
 # ffmpeg -f gdigrab -framerate ntsc -video_size 1920x1080 -i desktop  -f dshow -i audio="Microphone (Realtek High Definition Audio)" -vcodec libx264 -pix_fmt yuv420p -preset ultrafast D:\Movies\output.mp4
