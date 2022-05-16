0970cfc53f5b6b3532df8ab84ab9270c998ae8e3
zortazert <zortazert@matthewevan.xyz>
2022-05-16 18:07:45 -0500

    double stack 6 images can't do triple stack :(

--- video/gallerympv.py
+++ video/gallerympv.py
@@ -1,32 +1,35 @@
 import os
+from pathlib import Path
 
 dir = "C:\\SGZ_Pro\\Hobbys\\Media\\xy\\thumbnails\\"
+files2 = sorted(Path(dir).iterdir(), key=os.path.getmtime)
 files = os.listdir(dir)
-
 nums = list(range(int(len(files))))
 vidbrack = []
 for num in nums:
     vidbrack.append(f"[vid{num}]")
+def divide_chunks(l,n):
+    for i in range(0, len(l), n):
+        yield l[i:i +n]
+chunks = list(divide_chunks(vidbrack[1:],6))
 external_file = dir+files[0]
 t = ""
 num = 0
-lafvi = []
+lafvi = ""
+for file in files2:
+    external_file = external_file+f" --external-file={file}"
 for i, file in enumerate(files[1:]):
-    external_file = external_file+f" --external-file={dir+file}"
     if (i % 3):
         num = num+1
         t = t+f"[t{num}]"
-        lafvi = ''.join(vidbrack[num:num+3])+f"hstack=inputs=3[t{num}];"
-        print(lafvi)
-    elif i == 15:
+        lafvi = lafvi+''.join(chunks[num-1])+f"hstack=inputs=6[t{num}];"
+    elif i == 3:
         break
 inputs = i+1
 vidbrack = ''.join(vidbrack)
-lafvi = ''.join(lafvi)
-if t == "":
-    command = f'mpv --lavfi-complex="{vidbrack}hstack=inputs={inputs}[vo]" {external_file}'
-else:
-    command = f'mpv --lavfi-complex="{lafvi}{t} vstack [vo]" {external_file}'
-    #command = f'mpv --lavfi-complex="[vid1][vid2][vid3]hstack=inputs=3[t1];[vid4][vid5][vid6]hstack=inputs=3[t2];{t} vstack [vo]" {external_file}'
+command = f'mpv --lavfi-complex="{lafvi}{t} vstack [vo]" {external_file}'
 print(command)
-#os.system(command)
+os.system(command)
+
+#command = f'mpv --lavfi-complex="{vidbrack}hstack=inputs={inputs}[vo]" {external_file}'
+#command = f'mpv --lavfi-complex="[vid1][vid2][vid3]hstack=inputs=3[t1];[vid6][vid5][vid6]hstack=inputs=3[t2];{t} vstack [vo]" {external_file}'
