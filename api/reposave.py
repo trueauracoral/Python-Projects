c4eec05526ace1ed8938136e22e7b504122dd93e
zortazert <zortazert@matthewevan.xyz>
2022-05-16 09:52:36 -0500

    idk, I should be using emacs frick notepad++

--- api/reposave.py
+++ api/reposave.py
@@ -1,3 +1,15 @@
+# What do you need to run this script:
+# - python
+# - python requests
+# - a valid GitHub URL e.g. "https://github.com/<USER>"
+# - git
+# More information:
+# This script was created after TerminalForLife deleted his YouTube
+# channel all of it everything very sad. So while his github is still
+# working why not back it up. This includes forked repos and Public
+# Archives. Just everything. I might make a Codeberg account or
+# something like that to put all of his repos it will just be time
+# consuming at less there is some git command to create a repo.
 import requests
 import os
 
