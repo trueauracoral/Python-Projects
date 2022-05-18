5426417aed507d1a206b07f1f01fcde4ba8916c8
zortazert <zortazert@matthewevan.xyz>
2022-05-18 18:24:28 -0500

    I get like 8 times more memes now. This is the reason why I exist.
    To code weird pointless stuff. Or do other weird pointless stuff this
    makes me happy.

--- video/pics-to-vid.py
+++ video/pics-to-vid.py
@@ -10,17 +10,31 @@ headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/2012120
 data = requests.get(f"https://www.reddit.com/r/{subreddit}.json?limit=400",headers=headers).json()
 
 name = 0
+format = ".png"
 for image in data["data"]["children"]:
-    if image["data"]["url"].startswith("https://i.redd.it/") and image["data"]["url"].endswith(".png"):
-        name=name+1
-        print(name)
-        with open(str(name)+".png","wb") as f:
-            f.write(requests.get(image["data"]["url"]).content)
+    url = image["data"]["url"]
+    if url.startswith("https://i.redd.it/"):
+        if url.endswith(format):
+            name=name+1
+            print(name)
+            with open(str(name)+format,"wb") as f:
+                f.write(requests.get(image["data"]["url"]).content)
+        elif url.endswith(".jpg") or url.endswith(".jpeg"):
+            name=name+1
+            extension = "."+url.split("/")[3].split(".")[1]
+            print(name)
+            with open(str(name)+extension,"wb") as f:
+                f.write(requests.get(image["data"]["url"]).content)
+            os.system(f"ffmpeg -i {name}{extension} -preset ultrafast {name}{format}")
 
-os.system(f'ffmpeg -y -framerate 1 -i %d.png -vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1:color=black" -acodec copy input.mp4')
+os.system(f'ffmpeg -y -framerate 1 -i %d{format} -vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1:color=black" -acodec copy input.mp4')
 os.system(f'ffmpeg -y -i input.mp4 -i "{audio}" -filter_complex \" [1:0] apad \" -shortest output.mp4')
 
 for file in os.listdir():
     if file.endswith(".png"):
+        os.remove(file)
+    elif file.endswith(".jpg"):
+        os.remove(file)
+    elif file.endswith(".jpeg"):
         os.remove(file)
 os.remove("input.mp4")
