21cbe9cb2a14557fbea210d1134037898ca17d97
zortazert <zortazert@matthewevan.xyz>
2022-06-05 13:55:31 -0500

    Improvements in base64 to reddit-read and termpad stuff

--- video/reddit-read.py
+++ video/reddit-read.py
@@ -32,16 +32,25 @@ for i, post in enumerate(data["data"]["children"]):
                                     .replace("amp#39","")
         text = text.encode("ascii","ignore").decode()
         print(text)
-        os.system(texttospeech.replace("TEXT",text).replace("FILE",str(i)+".wav"))
+        #os.system(texttospeech.replace("TEXT",text).replace("FILE",str(i)+".wav"))
 
 os.system(texttospeech.replace("TEXT",text).replace("FILE","audio.wav"))
 text = text.replace(".","").split(" ")
 for i, word in enumerate(text):
     print(i)
     imagedata = requests.get(f"https://search.davidovski.xyz/api.php?q={word}&p=1&type=1").json()[0]["base64"]
-    decodedata = base64.b64decode(imagedata)
-    with open(str(i)+".jpeg","wb") as f:
-        f.write(decodedata)
+    os.system(f'ffmpeg -i "data:image/gif;base64,{imagedata}=" {str(i)}.png')
+#    decodedata = base64.b64decode(imagedata)
+#    try:
+#        with open(str(i)+".jpeg","wb") as f:
+#            f.write(decodedata)
+#    except:
+#        print("idk didn't work here")
 
-os.system(f'ffmpeg -y -framerate 1/0.5 -i %d.jpeg -r 25 -vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1:color=black" -pix_fmt yuv422p input.mp4')
+os.system(f'ffmpeg -y -framerate 1/0.3 -i %d.jpeg -r 25 -vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1:color=black" -pix_fmt yuv422p input.mp4')
 os.system(f'ffmpeg -y -i input.mp4 -i audio.wav output.mp4')
+
+for file in os.listdir():
+    if file.endswith(".jpeg"):
+        os.remove(file)
+os.remove("input.mp4")
