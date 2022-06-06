7ccff67f8cf3768cc2fd4354290797480f94c1e9
zortazert <zortazert@matthewevan.xyz>
2022-06-06 09:09:45 -0500

    improved the code. Ability to copy pastes

--- api/termpad.py
+++ api/termpad.py
@@ -9,26 +9,31 @@ import argparse
 browser = "palemoon"
 parser = argparse.ArgumentParser(description='Termpad curl python script')
 parser.add_argument('-g', '--get', type=str, metavar='ID', help='Get a pasted document')
-parser.add_argument('-o', '--open', type=str, metavar='ID', help='Open a pasted document in a browser.')
+parser.add_argument('-o', '--open', type=str, metavar='ID', help='Open a termpad link in a browser.')
 parser.add_argument('-p', '--paste', type=str, metavar='PATH', help='Paste an existing file.')
+parser.add_argument('-c', '--copy', action="store_true", default=False, help='Copy the link to the pasted file')
 args = parser.parse_args()
- 
 if args.get:
     if args.get == "":
         print("Please give a termpad URL/Paste ID")
     elif args.get.startswith("https://termpad.com"):
-        paste_id = args.get.split("/")[-1]
+        if "https://termpad.com/" in args.get:
+            paste_id = args.get.split("/")[-1]
+        else:
+            paste_id = args.get
         text = subprocess.getoutput(f"curl --silent https://termpad.com/raw/{paste_id}")
         print(text)
 elif args.paste:
-    if args.paste == "":
-        print("Please give a file")
+    if os.path.isfile(args.paste):
+        paste_url = subprocess.getoutput(f'curl --silent --data-binary @"{args.paste}" termpad.com')
+        print(paste_url)
+        if args.copy == True:
+            if platform.system() == "Windows"
+                os.system(f"echo {paste_url} | clip")
+            else:
+                os.system(f"echo '{paste_url}' | xclip -selection clipboard")
     else:
-        if os.path.isfile(args.paste):
-            paste_url = subprocess.getoutput(f'curl --silent --data-binary @"{args.paste}" termpad.com')
-            print(paste_url)
-        else:
-            print("This is not a file")
+        print("This is not a file")
 elif args.open:
     if args.open.startswith("https://termpad.com"):
         os.system(f"{browser} {args.open}")
