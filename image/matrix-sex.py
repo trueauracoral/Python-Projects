5ce4258a8ae3607aa77ab19430772508554b74e4
zortazert <zortazert@matthewevan.xyz>
2022-06-09 15:06:39 -0500

    Matrix sex improve

--- image/matrix-sex.py
+++ image/matrix-sex.py
@@ -1,10 +1,12 @@
 # Some code I got from:
 # https://github.com/rebane2001/txnor-server/blob/mane/sex.py
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
+import os
 
-file="C:\SGZ_Pro\Hobbys\Media\\remotecontrol.gif"
+image="C:\SGZ_Pro\Hobbys\Media\memes\pepes\\55.png"
+base_img ="C:\\SGZ_Pro\\Hobbys\\Media\\sex_base.png"
 font="C:\\SGZ_Pro\\Hobbys\\Media\\impact.ttf"
-img = Image.open(file).resize((350, 185)).convert("RGB")
+img = Image.open(image).resize((350, 185)).convert("RGB")
 
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("impact.ttf", 32)
@@ -14,3 +16,7 @@ draw.text((350 / 2, 185), "??? WTF ??? SEX ???", (255, 255, 255), font=font, anc
 stroke_fill=(0, 0, 0))
 
 img.save("output.png", "JPEG",quality=10)
+
+base = Image.open(base_img)
+base.paste(img,(5,100))
+base.save("output.png","PNG",quality=10)
