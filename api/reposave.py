a3b0241aa45cccbe28b37bd39cd50334745ecca4
zortazert <zortazert@matthewevan.xyz>
2022-06-21 18:50:50 -0500

    WIP slow progress on reposave.py

--- api/reposave.py
+++ api/reposave.py
@@ -13,20 +13,52 @@ parser.add_argument('-r', '--releases', action="store_true", default=False, help
 args = parser.parse_args()
 
 if args.github:
-    username = args.github
+    username = urlparse(args.github).path.replace("/","",1)
     api_url = "https://api.github.com"
-elif args.gitea:
-    if "/" in args.gitea:
-        username = urlparse(args.gitea).path.split("/")[1]
-        api_url = f"{urlparse(args.gitea).scheme}://{urlparse(args.gitea).netloc}/api/v1"
+    if args.releases:
+        get_releases = True
+        get_repos = False
     else:
-        print("Please tell what gitea server is this from e.g. https://domain.name/USER")
-        quit()
-data = requests.get(f"{api_url}/users/{username}/repos").json()
-print(username)
-
-os.mkdir(username)
-for repo in data:
-    print(repo["name"])
-    print(repo["description"])
-    os.system(f"cd {username} && git clone {repo['clone_url']}")
+        get_repos = True
+        get_releases = False
+elif args.gitea and "/" in args.gitea:
+    username = urlparse(args.gitea).path.replace("/","",1)
+    api_url = f"{urlparse(args.gitea).scheme}://{urlparse(args.gitea).netloc}/api/v1"
+    if args.releases:
+        get_releases = True
+        get_repos = False
+    else:
+        get_repos = True
+        get_releases = True
+else:
+    print("Please tell what gitea server is this from e.g. https://domain.name/USER")
+    quit()
+if get_repos == True:
+    data = requests.get(f"{api_url}/users/{username}/repos").json()
+    print(username)
+    
+    os.mkdir(username)
+    for repo in data:
+        print(repo["name"])
+        print(repo["description"])
+        os.system(f"cd {username} && git clone {repo['clone_url']}")
+if args.releases:
+    print(f"{api_url}/repos/{username}/releases")
+    data = requests.get(f"{api_url}/repos/{username}/releases").json()
+    assets=[]
+    for asset in data[0]['assets']:
+        assets.append(f"{asset['name']} - {asset['browser_download_url']}")
+    assets = '\n'.join(assets)
+    print(f"""Release Number: {data[0]['tag_name']}
+Release Name: {data[0]['name']}
+---
+[0] Tarball: {data[0]['tarball_url']}
+[1] Zipball: {data[0]['zipball_url']}
+Assets:
+[2] {assets}""")
+    try:
+        choice = int(input("Download: "))
+    except:
+        print("Has to be an integer")
+    if choice == 1:
+        
