9ae60595b14ffe2c0528c081dd2c75046df943ed
zortazert <zortazert@matthewevan.xyz>
2022-06-27 16:09:27 -0500

    Improvements to ffmpeg record script such as using argparse

--- video/ffmpeg-record.py
+++ video/ffmpeg-record.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
-# This script is windows only right now. When I switch to Linux I will
-# try and convert all ffmpeg options to be appropriate for a Linux
-# most likely X11 system. I am not an ffmpeg pro, I am just
-# researching options and using it over something like OBS for
-# things. Hopefully in the future I will put PeerTube livestreaming
-# and maybe LBRY if that is possible.
+# This is a wrapper script for ffmpeg to do various usefule things.
+# Read the help in order to understand in order to close it go to the
+# terminal window and press q on your keyboard. If you don't do this
+# there is some chance ffmpeg could mess with your recording.
+#
+# WARNING no gaurentee all these commands work on windows and linux
+# because I did not test. And most of these commands I got right off
+# of stack exchange and they add crazy options.
 import os
 import subprocess
 import re
 import sys
 import platform
+import argparse
 
-if len(sys.argv) == 1:
-    print("A missing or incorrect option was given")
+parser = argparse.ArgumentParser(description='Fffmpeg wrapper script.')
+parser.add_argument('-s', '--screen', action="store_true", default=False, help='Record screen')
+parser.add_argument('-sa', '--screen_audio', action="store_true", default=False, help='Record screen and audio')
+parser.add_argument('-a', '--audio', action="store_true", default=False, help='Record just audio')
+args = parser.parse_args()
 
-elif sys.argv[1] == "-s":
-    try:
-        output = input("Where do you want the recording to go: ")
-        print(platform.system())
-        if platform.system() == "Windows":
-            os.system(f"ffmpeg -f gdigrab -framerate 30 -i desktop {output}")
-        else:
-            os.system(f"ffmpeg -f x11grab -i :0.0 -f alsa -i hw:0 {output}")
-    except:
-        quit()
-
-elif sys.argv[1] == "-a":
+if args.screen:
+    output = input("Where do you want the recording to go: ")
+    print(platform.system())
+    if platform.system() == "Windows":
+        os.system(f"ffmpeg -f gdigrab -framerate 30 -i desktop {output}")
+    else:
+        os.system(f"ffmpeg -f x11grab -i :0.0 -f alsa -i hw:0 {output}")
+elif args.audio:
     devices = subprocess.getoutput("ffmpeg -list_devices true -f dshow -i dummy")
     data = re.findall('"(.+?)"',devices)
     for i,device in enumerate(data):
@@ -39,8 +41,7 @@ elif sys.argv[1] == "-a":
         os.system(command)
     except:
         quit()
-
-elif sys.argv[1] == "-sa":
+elif args.screen_audio:
     devices = subprocess.getoutput("ffmpeg -list_devices true -f dshow -i dummy")
     data = re.findall('"(.+?)"',devices)
     for i,device in enumerate(data):
@@ -56,10 +57,7 @@ elif sys.argv[1] == "-sa":
     # which monitor to use etc. But for now ffmpeg is basicly geussing
     # stuff.
     command = f'ffmpeg -f gdigrab -framerate ntsc -i desktop -f dshow -i audio="{pick}" -vcodec libx264 -pix_fmt yuv420p -preset ultrafast {output}'
-    try:
-        os.system(command)
-    except:
-        quit()
+    os.system(command)
 
 # Add an overlay thing on the lower right corner of the video
 # ffmpeg -i whole.mp4 -i meme.png -filter_complex "[0:v][1:v] overlay=W-w:H-h" -pix_fmt yuv420p -c:a copy wholeimage.mp4
