f328c7a9d9815103a56dd4d26cc3be2b1185ad4e
zortazert <zortazert@matthewevan.xyz>
2022-07-06 21:20:11 -0500

    stuff
    - file rename because 4chan's api TOS said you can use "4chan" in the
    title of anything you can create
    - page option when do a board so you can go to the page you want to go
    - print url of image because why not

--- api/chan.py
+++ api/chan.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import requests
 import argparse
 import html
@@ -10,6 +11,7 @@ viewer = "imcat"
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Terminal 4chan client')
     parser.add_argument('-b', '--board', type=str, metavar='BOARD', help='Select a board to view e.g. "g"')
+    parser.add_argument('-p', '--page', type=int, metavar='NUMBER', help='Go to a certain page number in board.')
     parser.add_argument('-l', '--list', action="store_true", default=False, help='List boards')
     return parser.parse_args()
 
@@ -26,8 +28,16 @@ def main():
             cleaner = re.compile('<.*?>')
             com = re.sub(cleaner, '', com)
             return com
-            
-        for thread in requests.get(f"https://a.4cdn.org/{args.board}/catalog.json").json()[0]["threads"]:
+
+        data = requests.get(f"https://a.4cdn.org/{args.board}/catalog.json").json()
+        if args.page:
+            if args.page >= 0 and args.page <= len(data):
+                num = args.page
+            else:
+                print("ERROR: Too big page number, printing out the first page")
+        else:
+            num = 0
+        for thread in data[0]["threads"]:
             print("---")
             title = f"{thread['name']} {thread['now']} No.{thread['no']}"
             if "sub" in thread:
@@ -37,9 +47,11 @@ def main():
 
             if thread["filename"] != "":
                 file = os.path.join(tempfile.gettempdir(),thread["filename"]+thread["ext"])
+                url = f"https://i.4cdn.org/{args.board}/{thread['tim']}{thread['ext']}"
                 with open(file,"wb") as f:
-                    f.write(requests.get(f"https://i.4cdn.org/{args.board}/{thread['tim']}{thread['ext']}").content)
+                    f.write(requests.get(url).content)
                 os.system(f'{viewer} "{file}"')
+                print(url)
             if "last_replies" in thread:
                 for reply in thread['last_replies']:
                     if "name" in reply:
