fbf7b04adb29f7ec737cf7a1ff94455d48ce4f7f
zortazert <zortazert@matthewevan.xyz>
2022-07-09 13:19:55 -0500

    improvements to fzf-twitch.py

--- fzf-scripts/fzf-twitch.py
+++ fzf-scripts/fzf-twitch.py
@@ -2,20 +2,30 @@
 import subprocess
 import os
 import platform
+import requests
 
 player = "mpv"
 fzf = "fzf --reverse < FILE"
-
 streams = f"""
+anthonywritescode
 theprimeagen
 rwxrob
 tsoding
+whitevault
+vimlark
+mortmort
 """
 
+live_streams = []
 with open("streams.txt","w") as f:
-    f.write(streams)
+    for stream in streams.splitlines():
+        # Very cool hack to not use twitch API
+        if stream != "" and 'isLiveBroadcast' in requests.get("https://www.twitch.tv/"+stream).content.decode('utf-8'):
+            live_streams.append(f"{stream} (Currently LIVE)")
+    f.write('\n'.join(live_streams))
 
 choice = subprocess.getoutput(fzf.replace("FILE","streams.txt"))
+
 if choice != "":
     subprocess.Popen(f"{player} https://twitch.tv/{choice}")
 os.remove("streams.txt")
