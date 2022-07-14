3f7600117ae7447f69f5773a80161d92a2be36c9
zortazert <zortazert@matthewevan.xyz>
2022-07-13 22:04:49 -0500

    WIP show upcomming stream and date nonesense

--- fzf-scripts/fzf-twitch.py
+++ fzf-scripts/fzf-twitch.py
@@ -4,6 +4,7 @@ import os
 import platform
 import requests
 import sys
+from datetime import datetime, timezone
 
 player = "mpv"
 fzf = "fzf --reverse < FILE"
@@ -17,19 +18,36 @@ whitevault
 mortmort
 """
 
-live_streams = []
-with open("streams.txt","w") as f:
-    for stream in streams.splitlines():
-        # Very cool hack to not use twitch API
-        if stream != "" and 'isLiveBroadcast' in requests.get("https://www.twitch.tv/"+stream).content.decode('utf-8'):
-            live_streams.append(f"{stream} (Currently LIVE)")
-    f.write('\n'.join(live_streams))
-
-if live_streams == []:
-    print("None of your followed streams are lived currently :(")
-    sys.exit(1)
-choice = subprocess.getoutput(fzf.replace("FILE","streams.txt"))
-
-if choice != "":
-    subprocess.Popen(f"{player} https://twitch.tv/{choice}")
-os.remove("streams.txt")
+def main():
+    # I got this from streamweasels.com under network in the f12
+    headers = {
+        "Authorization": "Bearer oll661c5pnlw0txizn436sbqxuig6q",
+        "Client-Id": "os2kmdts5tvcojd34pguyzsn3eyn5q"
+        }
+    channel_id = requests.get("https://api.twitch.tv/helix/users?login=whitevault", headers=headers).json()["data"][0]["id"]
+    url = f"https://api.twitch.tv/helix/schedule?broadcaster_id={channel_id}"
+    schedule = requests.get(url, headers=headers).json()
+    if "error" not in schedule:
+        date = datetime.strptime(schedule["data"]["segments"][0]["start_time"], "%Y-%m-%dT%H:%M:%SZ")
+        # Thank you Cringe, Doctor Tiberius Cringe
+        corrected = date.replace(tzinfo=timezone.utc).astimezone(tz=None).strftime("%A at %I:%M %p %z")
+        print(corrected)
+        print(type(date))
+    #live_streams = []
+    #with open("streams.txt","w") as f:
+    #    for stream in streams.splitlines():
+    #        # Very cool hack to not use twitch API
+    #        if stream != "" and 'isLiveBroadcast' in requests.get("https://www.twitch.tv/"+stream).content.decode('utf-8'):
+    #            live_streams.append(f"{stream} (Currently LIVE)")
+    #    f.write('\n'.join(live_streams))
+    #
+    #if live_streams == []:
+    #    print("None of your followed streams are lived currently :(")
+    #    sys.exit(1)
+    #choice = subprocess.getoutput(fzf.replace("FILE","streams.txt"))
+    #
+    #if choice != "":
+    #    subprocess.Popen(f"{player} https://twitch.tv/{choice}")
+    #os.remove("streams.txt")
+if __name__ == "__main__":
+    main()
