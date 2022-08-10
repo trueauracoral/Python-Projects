ad6c19c50f2985ea3b9f397102b2d42c8fe5a39a
zortazert <zortazert@matthewevan.xyz>
2022-08-10 15:14:02 -0500

    improvements

--- mini/git-build.py
+++ mini/git-build.py
@@ -2,14 +2,19 @@
 import subprocess
 import os
 import shutil
+import requests
+import urllib.parse
+import time
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name, get_lexer_for_filename
+from pygments.formatters import HtmlFormatter
 
-header = """
-<!DOCTYPE html>
+header = """<!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>Welcome to Zortazert's website!</title>
-<link rel="stylesheet" href="https://zortazert.codeberg.page/style/styles.css">
+<link rel="stylesheet" href="/style.css">
 <link rel="shortcut icon" href="https://zortazert.codeberg.page/images/Denshi-Meme-Server-Icon.png"/>
 </head>
 
@@ -18,6 +23,11 @@ header = """
 <p>
 <b>[ <a href='./animations.html'>Animations</a> <a href='./art.html'>Art Portfoldio</a> <a href='./internet-friends.html'>Internet Friends</a> <a href='./projects.html'>Projects</a> <a href='./donate.html'>Donate</a> ]</b>
 </p>"""
+compile_date = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(os.stat(os.getcwd()).st_mtime))
+end = f"""
+<h1>This was compiled for web view on {compile_date} and may not be the latest version.</h1>
+</body>
+</html>"""
 git_repo = "C:\\SGZ\\Coding-Projects\\Python-Projects"
 
 files = subprocess.getoutput(f"cd {git_repo} && git ls-files")
@@ -30,8 +40,11 @@ def linkify(file):
     print(f"{file} {num}")
     for s in file.split("/"):
         full += s + "/"
-        output.append(f"<a href='..{full}index.html'>{s}</a>")
-    return f"/<a href='{num*'../'}index.html'>{cwd_folder}</a>/"+"/".join(output)
+        output.append(f"<a href='{full}'>{s}</a>")
+    return f"/<a href='/'>{cwd_folder}</a>/"+"/".join(output)
+
+def commit(file):
+    return subprocess.getoutput(f'cd {git_repo} && git log --pretty=format:\'%h%x09%an%x09%ad%x09%s\' --no-decorate -1 -- {file}')[1:][:-1]
 
 for file in files.splitlines():
     original = os.path.join(git_repo, file)
@@ -47,17 +60,21 @@ for file in files.splitlines():
     with open(original, "r", encoding="utf-8") as f:
         try:
             text = f.read()
-        except:
-            print("This is most likely not a text file")
-    command = f'cd {git_repo} && git log --pretty=format:\'%h%x09%an%x09%ad%x09%s\' --no-decorate -1 -- {file}'
-    commit = subprocess.getoutput(command)
+            try:
+                lexer = get_lexer_for_filename(file)
+                formatter = HtmlFormatter(noclasses=True, style='monokai')
+                text = f"<p>{highlight(text, lexer, formatter)}</p>"
+            except Exception as e:
+                print(e)
+                text = f"<pre>{text}</pre>"
+        except Exception as e:
+            print(e)
     with open(new+".html", "w", encoding="utf-8") as f:
         f.write(f"""{header}
-<code>{commit}</code>
+<code>{commit(file)}</code>
 <h2>{linkify(file)}</h2>
-<pre>{text}</pre>
-</body>
-</html>""")
+{text}
+{end}""")
 
 for dir in [x[0] for x in os.walk(os.getcwd())]:
     files = []
@@ -71,14 +88,11 @@ for dir in [x[0] for x in os.walk(os.getcwd())]:
             files.append(f"<li><a href='{file}'>{file[:-5]}</a></li>")
     files = '\n'.join(files)
     with open(os.path.join(dir, "index.html"), "w") as f:
-        command = f'cd {git_repo} && git log --pretty=format:\'%h%x09%an%x09%ad%x09%s\' --no-decorate -1 -- {dir.replace(os.getcwd(),"")[1:]}'
-        commit = subprocess.getoutput(command)
         link = linkify(dir.replace(os.getcwd(), "").replace("\\", "/")[1:])
         f.write(f"""{header}
-<code>{commit}</code>
+<code>{commit(dir.replace(os.getcwd(),"")[1:])}</code>
 <h2>{link}</h2>
 <ul>
 {files}
 </ul>
-</body>
-</html>""")
+{end}""")
