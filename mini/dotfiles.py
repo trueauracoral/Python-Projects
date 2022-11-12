3d24e93bcb32c3fc3e6b8f7e8cc83b560e60d730
zortazert <zortazert@matthewevan.xyz>
2022-11-11 21:01:51 -0600

    Made dotfiles.py a bit better

--- mini/dotfiles.py
+++ mini/dotfiles.py
@@ -4,10 +4,11 @@ import os
 import platform
 import shutil
 
-if platform.system() == "Windows":
-    appdata = os.getenv('APPDATA')
-    localappdata = os.getenv('LOCALAPPDATA')
-configs = f"""
+def main():
+    if platform.system() == "Windows":
+        appdata = os.getenv('APPDATA')
+        localappdata = os.getenv('LOCALAPPDATA')
+    configs = f"""
 alacritty - {appdata}\\alacritty\\alacritty.yml
 emacs - {appdata}\\.emacs.d\\init.el
 mpv - {appdata}\\mpv\\mpv.conf
@@ -17,17 +18,32 @@ wezterm - C:\\SGZ_Pro\\z-Apps_Drivers\\wezterm\\wezterm.lua
 bash - C:\\Users\\Stanl\\.bashrc
 """
 
-git_repo = "C:\SGZ_Pro\Hobbys\coding-projects\Dots\\"
-
-for config in configs.splitlines():
-    if config == "":
-        pass
-    else:
+    git_repo = "C:\SGZ_Pro\Hobbys\coding-projects\Dots\\"
+    worked = []
+    for config in configs.splitlines():
+        if config == "":
+            continue
         config = config.split(" - ")
         oldpath = config[1]
         newpath = os.path.join(git_repo, config[0], os.path.basename(oldpath))
         if not os.path.exists(os.path.dirname(newpath)):
             os.mkdir(os.path.dirname(newpath))
-        print("OLD: "+oldpath)
-        print("NEW: "+newpath)
-        shutil.copy(oldpath,newpath)
+
+        with open(oldpath, "r", encoding="utf-8") as f:
+            oldpathtext = f.read()
+        with open(newpath, "r", encoding="utf-8") as f:
+            newpathtext = f.read()
+
+        if oldpathtext != newpathtext:
+            print("OLD: "+oldpath)
+            print("NEW: "+newpath)
+            shutil.copy(oldpath,newpath)
+            worked.append(newpath)
+
+    if len(worked) == 0:
+        print("No dotfile updates were found.")
+    else:
+        print(f"{len(worked)} configs were updated")
+
+if __name__ == "__main__":
+    main()
