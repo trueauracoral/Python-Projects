58fcb16a7a534d954bb319e879b48148d50952e8
zortazert <zortazert@matthewevan.xyz>
2023-06-24 13:14:36 -0500

    Sad life

--- scrape/gogoanime-dl.py
+++ scrape/gogoanime-dl.py
@@ -12,8 +12,11 @@ from http.cookiejar import LWPCookieJar
 requests.packages.urllib3.disable_warnings()
 import subprocess
 
+# Needs to get changed every week or month or two
+INSTANCE = "gogoanime.hu"
 DOWNLOADER = "yt-dlp"
-PLAYER = "MPV"
+# Put in here mpv ok
+PLAYER = "mpv"
 COMMAND = ['fzf', '--reverse']
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 7.0; 5060 Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36"
@@ -24,13 +27,12 @@ def get_arguments():
  
     parser.add_argument('-s', '--search', type=str, metavar='KEYWORD', help='Search Term')
     parser.add_argument('-n', '--filename', type=str, metavar='FILENAME', help='File Naming Convention')
+    parser.add_argument('-p', '--player', action="store_true", default=False, help='Play with player')
     parser.add_argument('link', nargs="?", type=str, metavar='URL', help='GOGO link')
  
     args = parser.parse_args()
  
     return args
-
-url = "https://ww4.gogoanimes.org/watch/pokemon-2019-harukanaru-aoi-sora-episode-1"
 
 def fetch(url):
     request = requests.get(url)
@@ -40,16 +42,20 @@ def fetch(url):
         return request.text
 
 def search(keyword):
-    data = fetch(f"https://ww5.gogoanimes.org/search?keyword={keyword}")
+    search_url = f"https://{INSTANCE}/search.html?keyword={keyword}"
+    data = fetch(search_url)
 
     soup = BeautifulSoup(data, "html.parser")
     # Grab all pages
     pages = soup.find("ul", {"class": "pagination-list"})
-    # How to get href's
-    # https://stackoverflow.com/questions/5815747/beautifulsoup-getting-href
-    # How to make for loop to list
-    # https://www.youtube.com/watch?v=kNTveFsQVHY
-    pagination = [f"https://ww5.gogoanimes.org/{x['href']}" for x in pages.find_all(href=True)]
+    if pages:
+        # How to get href's
+        # https://stackoverflow.com/questions/5815747/beautifulsoup-getting-href
+        # How to make for loop to list
+        # https://www.youtube.com/watch?v=kNTveFsQVHY
+        pagination = [f"https://{INSTANCE}/{x['href']}" for x in pages.find_all(href=True)]
+    else:
+        pagination = [search_url]
     search_data = []
     for page in pagination:
         data = fetch(page)
@@ -63,7 +69,7 @@ def search(keyword):
             search_data.append({
                 "name": f"{num+1}: {title}",
                 "title": title,
-                "link": f"https://ww5.gogoanimes.org{link}",
+                "link": f"https://{INSTANCE}{link}",
                 "image": image,
                 "alias": link.split("/")[-1]
             })
@@ -71,7 +77,7 @@ def search(keyword):
 
 def episode_list(name):
     #pagaste: https://github.com/justfoolingaround/animdl/blob/master/animdl/core/codebase/providers/gogoanime/__init__.py
-    page = fetch(f"https://gogoanime.cl/category/{name}")
+    page = fetch(f"https://{INSTANCE}/category/{name}")
     soup = BeautifulSoup(page, 'html.parser')
     id = soup.find("input", {"class": "movie_id"})['value'].zfill(2)
 
@@ -81,7 +87,7 @@ def episode_list(name):
     data = []
     for episode in episodes:
         number = int(episode.find('div', attrs={'class':'name'}).text.split(" ")[-1])
-        link = "https://gogoanime.cl"+episode.find('a')['href'][1:]
+        link = f"https://{INSTANCE}"+episode.find('a')['href'][1:]
         data.append({
             "number": str(number),
             "link": link
@@ -94,7 +100,10 @@ def fzf(data, key):
     if p.stdout == "":
         sys.exit("Please select something.")
     else:
-        index = int(p.stdout.split("\n")[0].split(":")[0]) - 1
+        try:
+            index = int(p.stdout) - 1
+        except:
+            index = int(p.stdout.split("\n")[0].split(":")[0]) - 1
         return data[index]
 
 def scrape(url):
@@ -111,27 +120,28 @@ def scrape(url):
     print(iframe)
     data = fetch(iframe)
 
-    soup = BeautifulSoup(data, "html.parser")
-    for li in soup.find_all("li"):
-        video = li.get("data-video")
-        if not video.startswith("https://"):
-            continue
-        host = urlparse(video).netloc
+    links = soup.find("div", {"class": "anime_muti_link"}).find_all("a")
+    for li in links:
+        video = li['data-video']
         print(video)
+        host = urlparse(video).netloc
+        # For now yt-dlp has support for mp4upload
         if host == "www.mp4upload.com":
-            url = video
-            vid_id = url.split(".html")[0].split("embed-")[1].split("/")[-1]
-
-            headers = {'referer': "https://mp4upload.com"}
-            data = {
-                'op': 'download2',
-                'id': vid_id,
-                'rand': '',
-                'referer': 'https://mp4upload.com/',
-                'method_free': '',
-                'method_premium': ''
-            }
-            request = requests.post(url, headers=headers, data=data, stream=True, verify=False)
+            request = video
+            # This code is so dogwater I didn't even make it
+            #url = video
+            #vid_id = url.split(".html")[0].split("embed-")[1].split("/")[-1]
+    
+            #headers = {'referer': "https://mp4upload.com"}
+            #data = {
+            #    'op': 'download2',
+            #    'id': vid_id,
+            #    'rand': '',
+            #    'referer': 'https://mp4upload.com/',
+            #    'method_free': '',
+            #    'method_premium': ''
+            #}
+            #request = requests.post(url, headers=headers, data=data, stream=True, verify=False)
         elif host == "fembed9hd.com":
             id = video.split("/")[-1]
             raw = requests.post("https://layarkacaxxi.icu/api/source/"+id).json()
@@ -147,12 +157,15 @@ def scrape(url):
         "episode": episode
     }
 
-def download(response, title):
+def download(response, title, play=False):
     invalid = '<>:"/\|?*'
     for char in invalid:
         title = title.replace(char, '')
     if type(response) == str:
-        os.system(f"{DOWNLOADER} {response} -o \"{title}.%(ext)s\"")
+        if play == True:
+            os.system(f"{PLAYER} --title=\"{title}\" {response}")
+        else:
+            os.system(f"{DOWNLOADER} {response} -o \"{title}.%(ext)s\"")
         return
     extension = mimetypes.guess_all_extensions(response.headers['Content-Type'], strict=False)[0]
     filename = f'{title}{extension}'
@@ -177,15 +190,22 @@ def download(response, title):
 
 def main():
     args = get_arguments()
+
     if args.link:
         data = scrape(args.link)
-        if args.filename:
-            title = args.filename % data
-        else:
-            title = data["title"]
-        download(data["response"], title)
     elif args.search:
-        print(fzf(episode_list(fzf(search(args.search), "name")["alias"]), "number"))
+        selection = fzf(episode_list(fzf(search(args.search), "name")["alias"]), "number")
+        data = scrape(selection["link"])
+    if args.filename:
+        title = args.filename % data
+    else:
+        title = data["title"]
+    print(data)
+    print(title)
+    if args.player:
+        download(data["response"], title, True)
+    else:
+        download(data["response"], title)
 
 if __name__ == "__main__":
     main()
