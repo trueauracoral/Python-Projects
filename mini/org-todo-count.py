17ffbb74fe4762bf75cbc39703bdff8177b58fe9
zortazert <zortazert@matthewevan.xyz>
2024-05-23 21:21:18 -0500

    org, yt, stackoverflow, gui dev

--- mini/org-todo-count.py
+++ mini/org-todo-count.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
-import re
+# Simple regex program to parse a org file for all TODO headers and
+# print them out nicely. I use it to put into wheelofnames.com
+import re # I am not a regex god. yet...
+import pyperclip # gigachad
 
 TODO_file = "C:\\SGZ_Pro\\Hobbys\\Writing\\Org\\todo.org"
 
 with open(TODO_file) as f:
     text = f.read()
 
-find = re.findall("\*.+?TODO",text)
-print(len(find))
+# Godly regex building the third temple with this one
+found = re.findall("\*.+?TODO .*",text)
+
+formatted = ""
+for i in found:
+    formatted += ' '.join(i.split(" ")[2:]) + "\n"
+
+print(formatted)
+pyperclip.copy(formatted)
+
+print(f"===LEFT===\nYou have {len(found)} items you need TODO")
--- rss/yt-community.py
+++ rss/yt-community.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+# This is broken lol
 # Script for Generating RSS from YT community tab
 # Example URL: https://www.youtube.com/@PaPaSea/community
 import re
@@ -9,11 +10,15 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
-sys.stdin.reconfigure(encoding="utf-8")
+#sys.stdin.reconfigure(encoding="utf-8")
+#
+#input_data = sys.stdin.read()
 
-input_data = sys.stdin.read()
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121201 icecat/17.0.1', "Content-Type": "text/html;charset=UTF-8"}
+data = requests.get("https://www.pokemon.com/us/pokemon-news", headers=HEADERS).text
 
-soup = BeautifulSoup(input_data, "html.parser")
+#soup = BeautifulSoup(input_data, "html.parser")
+soup = BeautifulSoup(data, "html.parser")
 name = json.loads(soup.find_all("script")[-6].text.split("var ytInitialData = ")[1].split(";")[-2])
 #print(json.dumps(name))
 posts = name["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][5]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"]
@@ -23,7 +28,7 @@ for post in posts:
 items = []
 
 json_feed = {
-  "title": "Pokemon News",
+  "title": "Youtube Community",
   "items": items,
 }
-#print(json.dumps(json_feed))
+print(json.dumps(json_feed))
